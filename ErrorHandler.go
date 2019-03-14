74cdebb9b2f3198eccc2edcdc397b582836df859
Asaf Niv <asafniv@disroot.org>
2019-03-14 12:38:04 +0200

    Better error reporting

--- ErrorHandler.go
+++ ErrorHandler.go
@@ -4,19 +4,21 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 	"fmt"
 )
-// This function is meant to run when the bot had an error and couldn't send an error message in the chat.
+// This function checks if there are errors and reports them.
 // err = the error
 // m = the message
-func handleError(err error, m tb.Message ) {
+func handleError(err error, sendError error, m tb.Message) { // notifyChat = if the bot should sent an error message in the chat **AND FAILED TO**.
 	if err != nil {
-		sentryError := raven.SetDSN("https://e2672e5909514951a621c35fc7818b2d:7b34b4bae174434fa31d356f2d0d446d@sentry.io/1415174")
-		if sentryError != nil {
-			fmt.Println("Couldn't set a sentry DSN")
-		}
-		raven.CaptureErrorAndWait(err, nil)
-		fmt.Println("Could not send message to chat: " + m.Chat.Title)
-		if m.Chat.Username != "" {
-			fmt.Println("Chat username: " + m.Chat.Username)
-		}
+		sendEvent(err)
 	}
+	if sendError != nil {
+		sendEvent(sendError)
+	}
+}
+func sendEvent(err error) {
+	sentryError := raven.SetDSN("https://e2672e5909514951a621c35fc7818b2d:7b34b4bae174434fa31d356f2d0d446d@sentry.io/1415174")
+	if sentryError != nil {
+		fmt.Println("Couldn't set a sentry DSN")
+	}
+	raven.CaptureErrorAndWait(err, nil)
 }
--- main.go
+++ main.go
@@ -17,41 +17,40 @@ func main() {
 		os.Exit(21)
 	}
 	b.Handle("/hello", func(m *tb.Message) {
-		_, err := b.Send(m.Chat, "HI THERE")
-		handleError(err, *m)
+		_, sendError := b.Send(m.Chat, "HI THERE")
+		handleError(err, sendError, *m)
 	})
 	b.Handle("/banme", func(m *tb.Message) {
 		user, _ := b.ChatMemberOf(m.Chat, m.Sender)
 		if err != nil {
-			_, err := b.Send(m.Chat, "ERRRRR")
-			handleError(err, *m)
+			_, sendError := b.Send(m.Chat, "ERRRRR")
+			handleError(err, sendError, *m)
 		}
 		err := b.Ban(m.Chat, user)
 		if err != nil {
-			_, err := b.Send(m.Chat, "ERRRR")
-			handleError(err, *m)
+			_, sendError := b.Send(m.Chat, "ERRRR")
+			handleError(err, sendError, *m)
 		}
 	})
 	// handle bans
 	b.Handle("/ban", func(m *tb.Message) {
 		replied := m.ReplyTo
-		sender, _ := b.ChatMemberOf(m.Chat, m.Sender)
-		user, _ := b.ChatMemberOf(m.Chat, m.Sender)
+		sender, err := b.ChatMemberOf(m.Chat, m.Sender)
+		handleError(err, nil, *m)
+		user, err := b.ChatMemberOf(m.Chat, m.Sender)
+		handleError(err, nil, *m)
 		if user.CanRestrictMembers || // check if the sender is an admin or the group creator.
 			tb.Creator == sender.Role {
-			user, _ := b.ChatMemberOf(m.Chat, replied.Sender)
+			user, err := b.ChatMemberOf(m.Chat, replied.Sender)
+			handleError(err, nil, *m)
+			err = b.Ban(m.Chat, user)
 			if err != nil {
-				_, err := b.Send(m.Chat, "ERRRRR")
-				handleError(err, *m)
-			}
-			err := b.Ban(m.Chat, user)
-			if err != nil {
-				_, err := b.Send(m.Chat, "ERRRR")
-				handleError(err, *m)
+				_, sendError := b.Send(m.Chat, "ERRRR")
+				handleError(err, sendError, *m)
 			}
 		} else { // runs when the sender doesn't have permission to ban users
-			_, err := b.Reply(m, "You don't have permission to ban users!")
-			handleError(err, *m)
+			_, sendError := b.Reply(m, "You don't have permission to ban users!")
+			handleError(err, sendError, *m)
 		}
 	})
 	b.Start()
