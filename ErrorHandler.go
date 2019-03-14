a90b71e492476779c4f37ee008d98e7ba122a7f2
Asaf Niv <asafniv@disroot.org>
2019-03-14 10:20:27 +0200

    Add sentry.io error reporting

--- ErrorHandler.go
+++ ErrorHandler.go
@@ -1,5 +1,6 @@
 package main
 import (
+	"github.com/getsentry/raven-go"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"fmt"
 )
@@ -8,6 +9,11 @@ import (
 // m = the message
 func handleError(err error, m tb.Message ) {
 	if err != nil {
+		sentryError := raven.SetDSN("https://e2672e5909514951a621c35fc7818b2d:7b34b4bae174434fa31d356f2d0d446d@sentry.io/1415174")
+		if sentryError != nil {
+			fmt.Println("Couldn't set a sentry DSN")
+		}
+		raven.CaptureErrorAndWait(err, nil)
 		fmt.Println("Could not send message to chat: " + m.Chat.Title)
 		if m.Chat.Username != "" {
 			fmt.Println("Chat username: " + m.Chat.Username)
