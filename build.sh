bd968233b14918c38f9bf068432b425cf7c58780
Asaf Niv <asafniv@disroot.org>
2019-03-14 08:39:31 +0200

    Faster builds

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ source token
 mkdir -p out
 go get ./...
 BUILDCONFIG="-X main.token=$TOKEN"
-CGO_ENABLED=0 go build -ldflags "${BUILDCONFIG}" -v -a -o out/${NAME}
+go build -ldflags "${BUILDCONFIG}" -v -a -o out/${NAME}
