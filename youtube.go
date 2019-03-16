6b1b7147fd53396c698250acd9813453aa092641
Asaf Niv <asafniv@disroot.org>
2019-03-16 17:07:49 +0200

    moar secret shite

--- youtube.go
+++ youtube.go
@@ -12,7 +12,7 @@ import(
 	"strings"
 )
 
-func downloadVideo(vidname string) {
+func downloadVideo(vidname string) int {
 	if strings.HasPrefix(vidname, "http://") ||
 		strings.HasPrefix(vidname, "https://")  {
 		// Filter vidname to get just the video ID
@@ -23,25 +23,32 @@ func downloadVideo(vidname string) {
 		if rawQ["v"] != nil {
 			fmt.Println("Found YouTube video ID from URL:", rawQ["v"][0])
 			vid := rawQ["v"][0]
-			ytdl(vid)
+			returnValue := ytdl(vid)
+			return returnValue
 		} else {
 			fmt.Println("Can't find YouTube video ID from URL")
 		}
 	} else {
 		vid := searchVideoID(vidname)
 		fmt.Println("Downloading YouTube video ID: " + vid)
-		ytdl(vid)
+		returnValue := ytdl(vid)
+		return returnValue
 	}
+	return 0
 }
 
 // vid = video ID
-func ytdl(vid string) {
+func ytdl(vid string) int {
 	mkCache := exec.Command("mkdir", ".cache")
-	dlCmd := exec.Command("youtube-dl", "https://youtu.be/" + vid, "-x", "--audio-format", "aac", "-o", ".cache/" + vid + ".mp4")
+	dlCmd := exec.Command("youtube-dl", "https://youtu.be/" + vid, "-f 'best[filesize<800M]'", "-x", "--audio-format", "aac", "-o", ".cache/" + vid + ".mp4")
 	mkCache.Run()
 	mkCache.Wait()
-	dlCmd.Run()
+	err := dlCmd.Run()
 	dlCmd.Wait()
+	if err != nil && strings.ContainsAny(err.Error(), "requested format not available") {
+		return 1
+	}
+	return 0
 }
 
 // This API key is taken by decompiling a "spotify downloader" app that actually downloads from youtube
