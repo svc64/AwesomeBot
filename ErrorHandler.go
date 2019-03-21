2e6f8afc8d3e34d1b61d500fbe034e46d7dfdd71
Asaf Niv <asafniv@asafniv.me>
2019-03-21 21:47:26 +0200

    ErrorHandler: comments

--- ErrorHandler.go
+++ ErrorHandler.go
@@ -22,23 +22,27 @@ var DSN = "https://e2672e5909514951a621c35fc7818b2d:7b34b4bae174434fa31d356f2d0d
 // m = the message
 func handleError(err error, sendError error, m tb.Message) { // notifyChat = if the bot should sent an error message in the chat **AND FAILED TO**.
 	if err != nil {
+		fmt.Println(err)
 		sendEvent(err, m)
 	}
 	if sendError != nil {
+		fmt.Println(sendError)
 		sendEvent(sendError, m)
 	}
 }
+// Sends an event to sentry
 func sendEvent(err error, m tb.Message) {
 	sentryError := raven.SetDSN(DSN)
 	if sentryError != nil {
 		fmt.Println("Couldn't set a sentry DSN")
 	}
+	// Add details about the chat
 	chatUsername := "Chat username: " + m.Chat.Username + "\n"
 	chatTitle := "Chat title: " + m.Chat.Title + "\n"
 	err = errors.New(chatUsername + chatTitle + err.Error())
-	raven.CaptureErrorAndWait(err, nil)
+	raven.CaptureErrorAndWait(err, nil) // Send it
 }
-
+// Handle an error that doesn't have anything to do with the chat, so there is no "m tb.Message" parameter.
 func handleGeneralError(err error) {
 	if err != nil {
 		sentryError := raven.SetDSN(DSN)
