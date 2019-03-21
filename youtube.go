5ef83b57586c46d5fe52708c85b9097272df2f48
Asaf Niv <asafniv@asafniv.me>
2019-03-21 14:41:36 +0200

    Add license in go files

--- youtube.go
+++ youtube.go
@@ -1,3 +1,13 @@
+/*
+ *     AwesomeBot
+ *     Copyright (C) 2019  Asaf Niv
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ */
+
 package main
 
 import(
