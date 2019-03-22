22c37049c88900cc1cb91377917fd7e1956be4d9
Asaf Niv <asafniv@asafniv.me>
2019-03-22 13:48:51 +0200

    Remove a space from the copyright notice

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 /*
  *     AwesomeBot
- *     Copyright (C) 2019  Asaf Niv
+ *     Copyright (C) 2019 Asaf Niv
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
