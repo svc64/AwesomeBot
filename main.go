1e120a01fdc7995aeb40770869e8f4cc171c22ff
Asaf Niv <asafniv@asafniv.me>
2019-03-23 20:11:01 +0200

    Explain stuff

--- main.go
+++ main.go
@@ -118,10 +118,11 @@ func main() {
 		handleError(err, nil, *m)
 		if sender.CanDeleteMessages ||
 			tb.Creator == sender.Role && bot.CanDeleteMessages {
+			// The ID grows by 1 every message so we'll use a for loop and add 1 every run
 			startID := m.ReplyTo.ID
 			endID := m.ID
 			for endID>=startID {
-				startIDString := strconv.Itoa(startID)
+				startIDString := strconv.Itoa(startID) // convert to string because params is a string map
 				params := map[string]string{
 					"chat_id":    strconv.FormatInt(m.Chat.ID, 10),
 					"message_id": startIDString,
